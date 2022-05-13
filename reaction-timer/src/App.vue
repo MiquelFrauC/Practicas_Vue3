d583c8be85227607b1cb06d573001cfa12ca0117
Miquel <miquel021002@gmail.com>
2022-05-13 14:40:44 +0200

    Reaction Timer finalizado.

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,15 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+
+  <Results v-if="showResults" :score="score"/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from "./components/Block.vue"
+import Results from "./components/Results.vue"
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block, Results},
+  data() {
+    return {
+      isPlaying: false, //Saber si el usuario está jugando o no.
+      delay: null, //Indica el tiempo de espera que hay entre que se pulsa el botón y se inicia el juego.
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start() {
+      //Especificar que el usuario está jugando
+      this.isPlaying = true
+
+      //Establecer el temporizador en un mínimo de 2 segundos más un tiempo aleatorio que puede aumentar el tiempo hasta los 7 segundos.
+      this.delay = 2000 + Math.random() * 5000
+      this.showResults = false
+    },
+
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
   }
 }
 </script>
@@ -20,7 +48,24 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+
+button {
+  background: #0FAF87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+      Click me
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true;
+            this.startTimer()
+        }, this.delay)
+    },
+    methods: {
+
+        //Crear dos métodos, uno que inicia un contador y otro que lo para, de esta forma
+        //se mide el tiempo que tarda el usuario en hacer click sobre el cuadrado cuando aparece.
+
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+
+        stopTimer() {
+            clearInterval(this.timer)
+            this.$emit("end", this.reactionTime)
+        }
+    }
+}
+</script>
+
+<style>
+    .block {
+        width: 400px;
+        border-radius: 20px;
+        background: #0faf87;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>

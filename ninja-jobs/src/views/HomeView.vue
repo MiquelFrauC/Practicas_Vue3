ba15310f616f7fc8c10d1269119d2eeb04e4b3e9
Miquel <miquel021002@gmail.com>
2022-05-16 11:06:59 +0200

    Initial commit.

---
+++ ninja-jobs/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos eum facilis similique tempora cum adipisci laboriosam velit perspiciatis accusantium nulla ducimus dolorum facere in molestias id dignissimos, sequi laborum mollitia quae qui pariatur unde iste? Qui unde consequatur, ipsam itaque, deserunt atque sequi illo laborum voluptates corporis obcaecati sapiente at, nesciunt veritatis incidunt facere rem maiores necessitatibus dolorum temporibus nulla sint totam deleniti quis? Nesciunt et cum, eveniet facilis corporis modi exercitationem odio recusandae officia dolores debitis, beatae id, accusamus vitae expedita in provident! Cumque modi maxime, et vel quod, in, esse alias rem iusto laborum sunt consequatur assumenda dolores.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos eum facilis similique tempora cum adipisci laboriosam velit perspiciatis accusantium nulla ducimus dolorum facere in molestias id dignissimos, sequi laborum mollitia quae qui pariatur unde iste? Qui unde consequatur, ipsam itaque, deserunt atque sequi illo laborum voluptates corporis obcaecati sapiente at, nesciunt veritatis incidunt facere rem maiores necessitatibus dolorum temporibus nulla sint totam deleniti quis? Nesciunt et cum, eveniet facilis corporis modi exercitationem odio recusandae officia dolores debitis, beatae id, accusamus vitae expedita in provident! Cumque modi maxime, et vel quod, in, esse alias rem iusto laborum sunt consequatur assumenda dolores.</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: { }
+}
+</script>
